54ca01e6b2cf20593f8b8336896aa473b1217b0f
yu-ka nishime <e195745@ie.u-ryukyu.ac.jp>
2020-01-21 23:32:53 +0900

    third

---
+++ src/jp/ac/uryukyu/ie/e195745/Judgment.java
@@ -0,0 +1,28 @@
+package jp.ac.uryukyu.ie.e195745;
+
+import java.util.List;
+
+//結果を判定するクラス
+public class Judgment {
+
+	 //手札がバーストしているか判定するメソッド
+    private static boolean isBusted(int point) {
+        if (point <= 21) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    //現在の合計ポイントを計算するメソッド
+    private static int sumPoint(List<Integer> list) {
+        int sum = 0;
+
+        for(int i =0;i < list.size();i++) {
+            sum = sum + toPoint(toNumber(list.get(i)));
+        }
+
+        return sum;
+    }
+
+}
